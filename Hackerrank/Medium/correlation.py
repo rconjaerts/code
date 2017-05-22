54e9bea6d78a7ec373921ab694912ed27148f9b3
Robrecht Conjaerts <robrecht_conjaerts@outlook.com>
2017-05-22 18:41:45 +0200

    correlation

---
+++ Hackerrank/Medium/correlation.py
@@ -0,0 +1,21 @@
+'''
+Problem: https://www.hackerrank.com/challenges/correlation-and-regression-lines-6
+Solved: True
+'''
+
+def calculateKPCoefficient():
+    # Problem with fixed input
+    res_phy = [15, 12, 8, 8, 7, 7, 7, 6, 5, 3]
+    res_hist = [10, 25, 17, 11, 13, 17, 20, 13, 9, 15]
+    
+    mean_phy = sum(res_phy)/len(res_phy)
+    mean_hist = sum(res_hist)/len(res_hist)
+    
+    diff_phy = [x-mean_phy for x in res_phy]
+    diff_hist = [x-mean_hist for x in res_hist]
+    
+    numerator = sum([x*y for x, y in zip(diff_phy, diff_hist)])
+    denominator = (sum([x**2 for x in diff_phy]))**0.5 * (sum([x**2 for x in diff_hist]))**0.5
+    return round(numerator/denominator, 3)
+    
+print(calculateKPCoefficient())
