1a502c77ce75a829461da6641d67c6151693259b
Robrecht Conjaerts <robrecht_conjaerts@outlook.com>
2017-06-19 21:07:57 +0200

    New code

---
+++ Hackerrank/Medium/the_power_sum.py
@@ -0,0 +1,19 @@
+'''
+Problem: https://www.hackerrank.com/challenges/the-power-sum
+Solved: True
+
+Notes: We could improve the performance by using dynamic programming
+'''
+
+x = int(input().strip())
+n = int(input().strip())
+
+def  findSum(curr_val, curr_sum):
+    if curr_sum+(curr_val**n) == x:
+        return 1
+    elif curr_val**n > x or curr_sum+(curr_val**n) > x:
+        return 0
+    else:
+        return findPaths(curr_val+1, curr_sum+curr_val**n)+findPaths(curr_val+1, curr_sum)
+    
+print(findSum(1, 0))
