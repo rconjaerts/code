41a22dc0ee31af678860ad1dc62f740cfa272acf
Robrecht Conjaerts <robrecht_conjaerts@outlook.com>
2017-09-11 20:17:39 +0200

    q_7 trees fix

--- Cracking the coding interview/Trees & Graphs/q_7.py
+++ Cracking the coding interview/Trees & Graphs/q_7.py
@@ -5,51 +5,49 @@ Solved: True
 '''
 
 class Node(object):
-    value = None
-    adjacents = []
-
     def __init__(self, value):
         self.value = value
+        self.incoming = []
+        self.outgoing = []
 
-    def add_adjacent(self, el):
-        self.adjacents.append(el)
-
-def find_build_order(projects, dependencies):
-    not_dependent = []
-    dependent = []
+def build_graph(projects, dependencies):
     all_nodes = {}
 
     for p in projects:
         all_nodes[p] = Node(p)
 
-    for d in dependencies:
-        if d[0] not in dependent and d[0] not in not_dependent:
-            not_dependent.append(d[0])
-        
-        if d[1] in not_dependent:
-            not_dependent.remove(d[1])
-            dependent.append(d[1])
-        elif d[1] not in dependent:
-            dependent.append(d[1])
-        
-        all_nodes[d[0]].add_adjacent(d[1])
-        print(all_nodes[d[0]])
+    for source, target in dependencies:
+        all_nodes[source].outgoing.append(target)
+        all_nodes[target].incoming.append(source)
+    return all_nodes
 
-    if not_dependent:
-        print(all_nodes['f'].adjacents)
-        for p in not_dependent:
-            for p2 in all_nodes[p].adjacents:
-                if p2 not in not_dependent:
-                    not_dependent.append(p2)
+def get_no_incoming(graph):
+    no_incoming = []
 
-        for p in projects:
-            if p not in not_dependent:
-                not_dependent.append(p)
-        return not_dependent
-    else:
-        return 'Not possible to create a build order'
+    for value, node in graph.iteritems():
+        if not node.incoming:
+            for out in node.outgoing:
+                graph[out].incoming.remove(node.value)
+            no_incoming.append(node)
 
-        
+    return no_incoming
+
+def find_build_order(projects, dependencies):
+    build_order = []
+    graph = build_graph(projects, dependencies)
+
+    while projects:
+        no_incoming = get_no_incoming(graph)
+        if no_incoming:
+            for node in no_incoming:
+                del graph[node.value]
+                projects.remove(node.value)
+                build_order.extend(node.value)
+        else:
+            return 'Not possible'
+
+    return build_order
+
 projects = ['a', 'b', 'c', 'd', 'e', 'f']
 dependencies = [('a', 'b'), ('f', 'b'), ('b', 'd'), ('f', 'a'), ('d', 'c')]
 
